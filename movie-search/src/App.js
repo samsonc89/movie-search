936566efafbb1d058f24470a1ae2696c7d0a3c23
Samson Cheung <samsonc89@yahoo.com>
2023-02-17 11:32:01 -0800

    Add function to search movies by title

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import { useEffect } from "react";
+
+//2d869879
+
+const API_URL = "http://www.omdbapi.com/?apikey=2d869879";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Hulk");
+  }, []);
   return <h1>App</h1>;
 };
 
